2e093a8376a6ac434f103c40577bcc26b8a6f8a8
Brayan Silvera <brayansilveraq@hotmail.com>
2020-03-17 15:56:04 -0500

    Revert "Clase 33"
    This reverts commit d871d22a1c6af06a946045262e17d2cc5e760003.

--- cride/cride-platzi-1-Codebase/cride/pharma/serializers/circles.py
+++ cride/cride-platzi-1-Codebase/cride/pharma/serializers/circles.py
@@ -3,14 +3,6 @@ from cride.pharma.models import Circle
 
 
 class CircleModelSerializer(serializers.ModelSerializer):
-
-    members_limit = serializers.IntegerField(
-        required=False,
-        min_value=10,
-        max_value=32000
-    )
-    is_limited = serializers.BooleanField(default=False)
-
     class Meta:
         model = Circle
         fields = [
@@ -24,22 +16,5 @@ class CircleModelSerializer(serializers.ModelSerializer):
             'verified',
             'is_public',
             'is_limited',
-            'members_limit'
 
         ]
-        read_only_fields = [
-            'is_public',
-            'verified',
-            'rides_offered',
-            'rides_taken',
-        ]
-    def validate(self, data):
-        #Ensure Both members_limit and is_limited are present
-        members_limit = data.get('members_limit', None)
-        is_limited = data.get('is_limited', False)
-
-        if is_limited ^ bool(members_limit):
-            raise serializers.ValidationError('If circle is limited, a member limit must be provied')
-        return data
-
-         
--- cride/cride-platzi-1-Codebase/cride/pharma/views/circles.py
+++ cride/cride-platzi-1-Codebase/cride/pharma/views/circles.py
@@ -1,20 +1,18 @@
 """Cicless views."""
 
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from cride.pharma.models import Circle, Membership
+from cride.pharma.models import Circle
 
 from cride.pharma.serializers.circles import CircleModelSerializer
-from cride.pharma.permissions.circles import IsCircleAdmin
 
 
-class CircleViewSet(mixins.CreateModelMixin,
-                    mixins.ListModelMixin,
-                    mixins.RetrieveModelMixin,
-                    viewsets.GenericViewSet,
-                    mixins.UpdateModelMixin):
+
+class CircleViewSet(viewsets.ModelViewSet):
     ##queryset = Circle.objects.all() listar sin condicion
-    serializer_class = CircleModelSerializer    
+    serializer_class = CircleModelSerializer
+    permission_classes = (IsAuthenticated,)
+    
 
 
     def get_queryset(self):
@@ -23,24 +21,4 @@ class CircleViewSet(mixins.CreateModelMixin,
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
-    def get_permissions(self):
-        #Assign permissions based on action
-        permissions = [IsAuthenticated]
-        if self.action in ['update', 'partial_update']:
-            permissions.append(IsCircleAdmin)
-        return [permission() for permission in permissions]
-
-    
-    def perform_create(self, serializer):
-        #Asignar Administrador al circulo
-        circle = serializer.save()
-        user = self.request.user
-        profile = user.profile
-        Membership.objects.create(
-            user=user,
-            profile=profile,
-            circle=circle,
-            is_admin=True,
-            remaining_invitations=10
-        )
     
