10611d8129e2ab0293c8d1ebbf56a88f143d6e23
Brayan Silvera <brayansilveraq@hotmail.com>
2020-04-26 01:35:42 -0500

    Revert commit Prueba templates

--- cride/cride-platzi-1-Codebase/cride/views.py
+++ cride/cride-platzi-1-Codebase/cride/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 def index(request):
-    doc_externo=open("templates/index.html")
+    doc_externo=open("cride/templates/page/index.html")
     plt=Template(doc_externo.read())
     doc_externo.close()
     ctx=Context()
