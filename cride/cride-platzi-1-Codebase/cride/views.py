9afa66982506849682532cffe02dedbb880877fd
Brayan Silvera <brayansilveraq@hotmail.com>
2020-04-26 01:51:33 -0500

    commit u

--- cride/cride-platzi-1-Codebase/cride/views.py
+++ cride/cride-platzi-1-Codebase/cride/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 def index(request):
-    doc_externo=open("cride/templates/page/index.html")
+    doc_externo=open("age/index.html")
     plt=Template(doc_externo.read())
     doc_externo.close()
     ctx=Context()
